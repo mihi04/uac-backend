2d66dcd8d0395e047c1bdbc731be48bfd906cee8
mihi04 <khushi.mihi04@gmail.com>
2026-04-03 02:48:06 +0530

    superuser migration script added

--- apps/users/management/commands/create_default_superuser.py
+++ apps/users/management/commands/create_default_superuser.py
@@ -2,7 +2,6 @@ import os
 from django.core.management.base import BaseCommand
 from django.contrib.auth import get_user_model
 
-
 User = get_user_model()
 
 class Command(BaseCommand):
@@ -11,14 +10,14 @@ class Command(BaseCommand):
     def handle(self, *args, **kwargs):
         email = os.environ.get('SUPERUSER_EMAIL')
         password = os.environ.get('SUPERUSER_PASSWORD')
-        
+
         if not email or not password:
             self.stdout.write('SUPERUSER_EMAIL or SUPERUSER_PASSWORD not set, skipping.')
             return
-            
+
         if User.objects.filter(email=email).exists():
             self.stdout.write(f'Superuser {email} already exists, skipping.')
             return
-            
+
         User.objects.create_superuser(email=email, password=password)
         self.stdout.write(f'Superuser {email} created successfully.')
